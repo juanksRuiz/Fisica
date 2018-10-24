c50693e91509f93b8414840d27cfff791f4b3ce8
juanksRuiz <33558038+juanksRuiz@users.noreply.github.com>
2018-10-24 12:45:12 -0500

    Corrigiendo más errores

--- AyudaTracker.py
+++ AyudaTracker.py
@@ -4,7 +4,9 @@ def separarDatos(path):
     #Primero seleccionar en Tracker los datos COMPLETOS
     #path: direccion del .txt donde estan los datos
 
+
     # con indices i se accede/cambia por referencia los valores de una lista
+
 
     f = open(path,'r')
     datos = f.readlines()
@@ -16,33 +18,35 @@ def separarDatos(path):
         todo.append(datos[i].split('&'))
     #print "datos string separados"    
     #print todo
-
+    
+    cont = 0
     for linea in todo:
         #limpiando lectura de archivo
         if len(linea) != len(todo[0]):
             todo.remove(linea)
         else:
             for i in range(len(linea)):
-                if linea[i] == linea[-1]:
+		if linea[i] == '\n':
+		    todo.pop(cont)
+                elif linea[i] == linea[-1]:
                     a = linea[i][:-1]
+		    #print a
                     linea[i] = float(a)
                 else:
                     linea[i] = float(linea[i])
+
+	cont = cont + 1
+
+    final = []
+    for i in range(len(todo)):
+	if '\n' not in todo[i]:
+	    final.append(todo[i])
+
+
+
+
     #print "Datos de salida"
-    #print todo
+    #print final
     #print "Se ha termindo de corregir los datos"
     f.close()
-    return todo
-
-def extraerDatos(L,idx):
-    #Retorna una lista con los valores del mismo tipo(OJALÁ) de una lista de listas
-    #L: lista de listas
-    #idx: indice de los datos en cada lista:
-
-    a = []
-    for l in L:
-        for i in range(len(l)):
-            if i == idx:
-                a.append(l[i])
-
-    return a
+    return final
