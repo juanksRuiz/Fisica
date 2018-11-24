be0ed28112804d870f9c5b05769884d1ef9e301c
LAPTOP-FDUE24NN\juank <juancamilo.ruiz@urosario.edu.co>
2018-11-24 12:39:51 -0500

    Funcion times terminada

--- Taller3.py
+++ Taller3.py
@@ -10,19 +10,29 @@ def timesParticles(lp):
     #lp: lista de particulas
     dic = {}
     for i in lp:
+        #print "-----------------------"
+        #print "i: ",i.nombre
         times = []
         for j in lp:
-            rij = []
-            vij = []
-            for k in range(len(i.posicion)):
-                rij.append(i.posicion[k] - j.posicion[k])
-                vij.append(i.velocidad[k] - j.velocidad[k])
-            normRij = (prodEscalar(rij,rij))**(1./2.)
-            normVij = (prodEscalar(vij,vij)**(1./2.))
-            tij = (-prodEscalar(rij,vij) - ( (prodEscalar(rij,vij))**2 - (normVij**2)*((normRij**2) - (i.sigma**2)) )**1./2.)/normVij**2
-            times.append(tij)
-        dic[i] = times
+            distintos = False
+            if i != j:
+                #print "DISTINTOS: ",DISTINTOS
+                #print "Creando tiempo entre " +i.nombre + ' y ' +j.nombre
+                distintos = True
+                #print "distintos: ",distintos
+                rij = []
+                vij = []
+                for k in range(len(i.posicion)):
+                    rij.append(i.posicion[k] - j.posicion[k])
+                    vij.append(i.velocidad[k] - j.velocidad[k])
 
+                normRij = (prodEscalar(rij,rij))**(1./2.)
+                normVij = (prodEscalar(vij,vij)**(1./2.))
+                tij = (-prodEscalar(rij,vij) - ( (prodEscalar(rij,vij))**2 - (normVij**2)*((normRij**2) - (i.sigma**2)) )**1./2.)/normVij**2
+                times.append([j.nombre,tij])
+            #print 'confirmando distintos: ',distintos
+                dic[i.nombre] = times
+                
     return dic
 
 def timesWall(lp,Lx,Ly):
@@ -33,41 +43,76 @@ def timesWall(lp,Lx,Ly):
     dic = {}
     times = []
     for i in lp:
-        td = (Lx-(i.sigma /2.)- i.posicion[0])/i.velocidad[0]
-        ti = ((i.sigma/2.) - i.posicion[0])/i.velocidad[0]
-        tar = (Ly-(i.sigma /2.)- i.posicion[1])/i.velocidad[1]
-        tab = ((i.sigma/2.) - i.posicion[1])/i.velocidad[1]
-        times.append(td)
-        times.append(ti)
-        times.append(tar)
-        times.append(tab)
-        dic[i] = times
+        if i.velocidad[0] != 0:
+            td = (Lx-(i.sigma /2.)- i.posicion[0])/i.velocidad[0]
+            ti = ((i.sigma/2.) - i.posicion[0])/i.velocidad[0]
+            if td > 0:
+                times.append(['der',td])
+            if ti > 0:
+                times.append(['izq',ti])
+        if i.velocidad[1] != 0:
+            tar = (Ly-(i.sigma /2.)- i.posicion[1])/i.velocidad[1]
+            tab = ((i.sigma/2.) - i.posicion[1])/i.velocidad[1]
+            if tar >0:
+                times.append(['arriba',tar])
+            if tab >0:
+                times.append(['abajo',tab])
+        dic[i.nombre] = times
         times = []
     return dic
 
-def  times
-        
-        
-              
-        
-          
-          
+def times(lp,Lx,Ly):
+    d = {}
+    dPart = timesParticles(lp)
+    dWall = timesWall(lp,Lx,Ly)
+
+    if (len(dPart) == 0 and len(dWall) != 0):
+        d = dWall
+    elif len(dPart) != 0 and len(dWall) == 0:
+        d = dPart
+    else:
+        for k in dPart.keys():
+            for p in dWall.keys():
+                if k == p:
+                    d[k] = dPart[k] + dWall[k]
+                
+    return d
  
   
 class Particle(object):
-    def __init__(self,posicion,velocidad,diametro):
+    def __init__(self,posicion,velocidad,diametro,idx):
         #posicion: lista de dos componentes: x y y
         #velocidad: lista de dos componentes: x y y
+        #idx: indice para el nombre
         self.posicion = posicion
         self.velocidad = velocidad
         self.sigma = diametro
+        self.nombre = 'p' + str(idx)
 
     def move(self,t):
       for i in range(len(self.posicion)):
-        self.posicion[i] = self.posicion[i] + t*self.velocidad(i)
+        self.posicion[i] = self.posicion[i] + t*self.velocidad[i]
   
     
 
     
 Lx = 10
 Ly = 10
+
+p1 = Particle([2,5],[2,0],0.5,1)
+p2 = Particle([7,2],[0,0.5],0.5,2)
+p3 = Particle([8,8],[-1,-1],0.5,3)
+lp = [p1,p2,p3]
+
+for p in lp:
+    print p.nombre
+    print p.posicion
+    print p.velocidad
+    print "------------------"
+print times(lp,Lx,Ly)
+##
+##print "Posicion inicial de particula: ",p1.posicion
+##p1.move(1)
+##print "Posicion final de particula: ",p1.posicion
+ 
+
