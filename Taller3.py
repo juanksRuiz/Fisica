7fb900962aaa4dea7376f31bcbe71e05379251b5
LAPTOP-FDUE24NN\juank <juancamilo.ruiz@urosario.edu.co>
2018-11-24 21:42:18 -0500

    Funcion ChangeMomentum terminada, faltan pruebas

--- Taller3.py
+++ Taller3.py
@@ -77,8 +77,77 @@ def times(lp,Lx,Ly):
                     d[k] = dPart[k] + dWall[k]
                 
     return d
- 
-  
+def dist(u,v):
+    if len(u)!= len(v):
+        return 'ERROR EN LAS DIMENSIONES'
+    dx = u[0] - v[0]
+    dy = u[1] - v[1]
+    return ((dx**2)+(dy**2))**1./2.
+
+def Rij(pi,pj):
+    rij = []
+    for i in range(len(pi)):
+        rij[i] = pi.posicion[i] - pj.posicion[i]
+
+    return rij
+
+def Vij(pi,pj):
+    vij = []
+    for i in range(len(pi)):
+        vij[i] = pi.velocidad[i] - pj.velocidad[i]
+
+    return vij
+
+def changeMomentum(Lx,Ly,p1,p2):
+    #p1: particula i
+    #p2: particula j
+    #asumiendo que hay colision:
+    # Si choca con algún muro
+
+    #Con el muro de la derecha
+    if (p1.posicion[0] + p.sigma/2. >= Lx):
+        p1.velocidad[0] = -p1.velocidad[0]
+    if(p2.posicion[0] + p2.sigma/2. >= Lx):
+        p2.velocidad[1] = - p2.velocidad[1]
+
+    #Con el muro de la izquierda
+    if (p1.posicion[0] - p.sigma/2. <= 0):
+        p1.velocidad[0] = -p1.velocidad[0]
+    if(p2.posicion[1] - p2.sigma/2. <= 0):
+        p2.velocidad[1] = - p2.velocidad[1]
+
+    #Con el muro de arriba
+    if (p1.posicion[1] + p1.sigma/2. >= Ly):
+        p1.velocidad[1] = -p1.velocidad[1]
+    if(p2.posicion[1] + p2.sigma/2. >= Ly):
+        p2.velocidad[1] = - p2.velocidad[1]
+
+    #Con el muro de abajo
+    if (p1.posicion[1] - p.sigma/2. <= 0):
+        p1.velocidad[1] = -p1.velocidad[1]
+    if(p2.posicion[1] - p2.sigma/2. <= 0):
+        p2.velocidad[1] = - p2.velocidad[1]
+
+    #Si hay choque entre las particulas
+    if dist(p1.posicion,p2.posicion) == p1.sigma:
+        rij = Rij(p1,p2)
+        vij = Vij(p1,p2)
+        #rij <--- rij/sigma
+        for i in range(len(rij)):
+            rij[i] = rij[i]/p1.sigma
+    
+        coef = prodEscalar(vij,rij)
+        deltaV = []
+        for i in range(len(rij)):
+            deltaV[i] = -coef*rij[i]
+
+        for i in range(len(p1.velocidad)):
+            p1.velocidad[i] = p1.velocidad[i] + deltaV[i]
+            p2.velocidad[i] = p2.velocidad[i] - deltaV[i]
+            
+        
+
+
 class Particle(object):
     def __init__(self,posicion,velocidad,diametro,idx):
         #posicion: lista de dos componentes: x y y
@@ -104,11 +173,12 @@ p2 = Particle([7,2],[0,0.5],0.5,2)
 p3 = Particle([8,8],[-1,-1],0.5,3)
 lp = [p1,p2,p3]
 
-for p in lp:
-    print p.nombre
-    print p.posicion
-    print p.velocidad
-    print "------------------"
+
+def getParticle(i):
+    for p in lp:
+        if 'p'+str(i) == p.nombre:
+            return p
+
 print times(lp,Lx,Ly)
 ##
 ##print "Posicion inicial de particula: ",p1.posicion
